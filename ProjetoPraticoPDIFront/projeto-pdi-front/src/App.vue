0b548653595b355fadb7197368657b311b9f4f37
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2022-09-08 12:37:18 -0300

    Update

---
+++ ProjetoPraticoPDIFront/projeto-pdi-front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="Projeto Prático PDI"/>
+  <GetPessoas msgPessoas="Projeto Prático PDI"/>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import GetPessoas from './components/GetPessoas.vue';
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    GetPessoas
+}
+}
+</script>
+
---
+++ ProjetoPraticoPDIFront/projeto-pdi-front/src/components/GetPessoas.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+    <h1>{{ msgPessoas }}</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    NOME
+                </th>
+                <th>
+                    E-MAIL
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="pessoa in pessoas" v-bind:key="pessoa.id" >
+                <td>
+                    {{pessoa.id}}
+                </td>
+                <td>
+                    {{pessoa.nome}}
+                </td>
+                <td>
+                    {{pessoa.email}}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+    import axios from 'axios';
+export default {
+  name: 'GetPessoas',
+  props: {
+    msgPessoas: String
+  },
+  data: () => {
+    return {
+        pessoas: []
+    }
+  },
+  methods: {
+    lista: (scope) => {
+        axios.get('https://localhost:7060/ProjetoPraticoPDI/GetPessoas').then((res)=>{
+            console.log("Oiiiiii")
+            console.log(res)
+            scope.pessoas = res.data
+        })
+    }    
+  },
+  created() {
+    this.lista(this)
+  }
+}
+</script>
