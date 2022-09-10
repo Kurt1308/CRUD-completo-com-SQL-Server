a14416f73b7f7f737dec604223e063cc269c8035
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2022-09-10 07:37:25 -0300

    Configuração baseURL

--- ProjetoPraticoPDIFront/front-end-pdi/src/sources/api.js
+++ ProjetoPraticoPDIFront/front-end-pdi/src/sources/api.js
@@ -1,7 +1,7 @@
 import axios  from 'axios';
 
 const api = axios.create({
-    baseURL:"https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/api"
+    baseURL:"https://localhost:7060/swagger/v1/swagger.json"
 });
 
 export default api;
